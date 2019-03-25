8cd0bdd3366f6a947065ad01e885419dae58152c
Lucie_C <lucie.chasan@etu.u-bordeaux.fr>
2019-03-25 14:56:47 +0100

    Fin de la parite Add/Substract

---
+++ morphology/substract.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <morphology.h>
+
+void
+usage(char* s)
+{
+  fprintf(stderr,"%s <ims-1> <ims-2> <imd>\n",s);
+  exit(EXIT_FAILURE);
+}
+
+#define PARAM 3
+int
+main(int argc, char* argv[])
+{
+  if(argc != PARAM+1) usage(argv[0]);
+  pnm in1 = pnm_load(argv[1]);
+  pnm in2 = pnm_load(argv[2]);
+
+  int cols = pnm_get_width(in1);
+  int rows = pnm_get_height(in1);
+
+  pnm out = pnm_new(cols ,rows , PnmRawPpm);
+
+  unsigned short * channel_1 = pnm_get_channel(in1, NULL, 0);
+  unsigned short * channel_2 = pnm_get_channel(in2, NULL, 0);
+  unsigned short * channel_out = pnm_get_channel(out, NULL, 0);
+
+  short tmp;
+
+  for (int i = 0; i < cols *rows; i++){
+    tmp = channel_1[i] - channel_2[i];
+    if (tmp < 0) channel_out[i] = 0;
+    else channel_out[i] = (unsigned short) tmp;
+  }
+
+  pnm_set_channel(out, channel_out, 0);
+  pnm_set_channel(out, channel_out, 1);
+  pnm_set_channel(out, channel_out, 2);
+
+  pnm_save(out,PnmRawPpm,argv[3]);
+
+  pnm_free(in1);
+  pnm_free(in2);
+  pnm_free(out);
+
+  free(channel_1); free(channel_2); free(channel_out);
+
+  return EXIT_SUCCESS;
+}
